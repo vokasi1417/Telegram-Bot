937e25c4ba45c321d3ebb52889abe639e304a559
HolyHovorun <52285345+HolyHovorun@users.noreply.github.com>
2020-05-08 00:31:03 +0300

    echo bot 2.0

--- bot.py
+++ bot.py
@@ -1,16 +1,17 @@
 import telebot
+import config
 
-bot = telebot.TeleBot('TOKEN')
+bot = telebot.TeleBot(config.token)
 
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-    bot.reply_to(message, "Hey, how are u?")
+    bot.send_message(message.chat.id, "Hey, how are u?")
 
 
-@bot.message_handler(func=lambda m: True)
-def echo_all(message):
-    bot.reply_to(message, message.text)
+@bot.message_handler(content_types=["text"])
+def repeate_all_messages(message):
+    bot.send_message(message.chat.id, message.text)
 
 
 bot.polling()
